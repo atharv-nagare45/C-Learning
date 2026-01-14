625e2e3e037e9ba4fa573e073cd2f2d55fc50dc8
atharv-nagare45 <atharvnagare79@gmail.com>
2026-01-14 08:54:44 -0800

    Add C keywords list as a comment
    Added a comment listing standard C keywords.

--- 04_Keywords.c
+++ 04_Keywords.c
@@ -4,4 +4,16 @@ int main()
 {
     printf("Some words are reserved by c programing language");
     return 0;
+
 }
+/*
+Here is the standard list of 32 C keywords:
+auto        break       case        char
+const       continue    default     do
+double      else        enum        extern
+float       for         goto        if
+int         long        register    return
+short       signed      sizeof      static
+struct      switch      typedef    union
+unsigned    void        volatile    while
+*/
