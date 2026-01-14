3605665f2093c960f9c90b161cb4231298ff393a
atharv-nagare45 <atharvnagare79@gmail.com>
2026-01-14 09:00:18 -0800

    Refactor printf statements for clarity
    Fixed printf statements to separate outputs for a and b.

--- 08_Arithmetics.c
+++ 08_Arithmetics.c
@@ -5,8 +5,11 @@ int main()
     int a = 4;
     int b = 3;
     int c = a + b;
-    printf("The value of a is %d \n and the value of b is %d\n and the sum of both number is %d \n", a, b, c);
+    printf("The value of a is %d \n"a);
+    printf("The value of b is %d \n",b);
+    printf("The sum of both number is %d \n",c);
     // modulus operator is use to get a remainder
     printf("The remainder of calculation if we divide a by b is %d", a % b);
     return 0;
+
 }
